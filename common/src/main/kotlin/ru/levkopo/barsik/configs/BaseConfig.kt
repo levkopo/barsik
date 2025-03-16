4b33e21587c420c001c882ec7cb4c56d80b8f8cf
levkopo <levkopoag@gmail.com>
2025-03-16 22:57:20 +0500

    feat: settings saving

--- common/src/main/kotlin/ru/levkopo/barsik/configs/BaseConfig.kt
+++ common/src/main/kotlin/ru/levkopo/barsik/configs/BaseConfig.kt
@@ -1,29 +1,61 @@
 package ru.levkopo.barsik.configs
 
+import java.io.File
+import java.util.prefs.Preferences
 import kotlin.reflect.KProperty
 
 abstract class BaseConfig {
-    protected fun stringProperty(defaultValue: String) = PropertyDelegate(defaultValue)
-    protected fun doubleProperty(defaultValue: Double) = PropertyDelegate(defaultValue)
-    protected fun intProperty(defaultValue: Int) = PropertyDelegate(defaultValue)
-    protected fun floatProperty(defaultValue: Float) = PropertyDelegate(defaultValue)
-    protected fun shortProperty(defaultValue: Short) = PropertyDelegate(defaultValue)
+    private val preferences = Preferences.userRoot().node(this.javaClass.simpleName)
 
-    protected class PropertyDelegate<T>(private val defaultValue: T) {
+    protected fun stringProperty(defaultValue: String) = PropertyDelegate(
+        defaultValue = defaultValue,
+        resolveSavedValue = { preferences.get(it, defaultValue) },
+        save = { name, value ->  preferences.put(name, value) }
+    )
+
+    protected fun doubleProperty(defaultValue: Double) = PropertyDelegate(
+        defaultValue = defaultValue,
+        resolveSavedValue = { preferences.getDouble(it, defaultValue) },
+        save = { name, value ->  preferences.putDouble(name, value) }
+    )
+
+    protected fun intProperty(defaultValue: Int) = PropertyDelegate(
+        defaultValue = defaultValue,
+        resolveSavedValue = { preferences.getInt(it, defaultValue) },
+        save = { name, value ->  preferences.putInt(name, value) }
+    )
+
+    protected fun floatProperty(defaultValue: Float) = PropertyDelegate(
+        defaultValue = defaultValue,
+        resolveSavedValue = { preferences.getFloat(it, defaultValue) },
+        save = { name, value ->  preferences.putFloat(name, value) }
+    )
+
+    protected fun shortProperty(defaultValue: Short) = PropertyDelegate(
+        defaultValue = defaultValue,
+        resolveSavedValue = { preferences.getInt(it, defaultValue.toInt()).toShort() },
+        save = { name, value ->  preferences.putInt(name, value.toInt()) }
+    )
+
+    protected class PropertyDelegate<T>(
+        private val defaultValue: T,
+        private val resolveSavedValue: (name: String) -> T?,
+        private val save: (name: String, T) -> Unit
+    ) {
         private var currentValue: T? = null
 
         operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
             var currentValue = currentValue
-            if(currentValue == null) {
-                return defaultValue
+            if (currentValue == null) {
+                currentValue = resolveSavedValue(property.name) ?: defaultValue
             }
 
-            return currentValue
+            return currentValue!!
         }
 
         operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
             currentValue = value
-            println("$value has been assigned to '${property.name}' in $thisRef.")
+            save(property.name, value)
         }
     }
 }
