8e2d37ff31c794c58572c03244c57b567c6d900c
levkopo <levkopoag@gmail.com>
2025-03-17 21:17:09 +0500

    fix: Нормальный размер иконки

--- common/src/main/kotlin/ru/levkopo/barsik/configs/ServerConfig.kt
+++ common/src/main/kotlin/ru/levkopo/barsik/configs/ServerConfig.kt
@@ -1,7 +1,7 @@
 package ru.levkopo.barsik.configs
 
 object ServerConfig: BaseConfig() {
-    var linuxBoxIp by stringProperty("localhost")
+    var linuxBoxIp by stringProperty("10.8.0.5")
     var linuxBoxOrbPort by stringProperty("9999")
     var linuxBoxUsername by stringProperty("at")
     var linuxBoxPassword by stringProperty("gfhjkm")
