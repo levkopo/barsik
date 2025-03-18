40c5d47e8910b966879cb880171a8bd0e1345593
levkopo <levkopoag@gmail.com>
2025-03-18 20:04:52 +0500

    fix: default ip

--- common/src/main/kotlin/ru/levkopo/barsik/configs/ServerConfig.kt
+++ common/src/main/kotlin/ru/levkopo/barsik/configs/ServerConfig.kt
@@ -1,7 +1,7 @@
 package ru.levkopo.barsik.configs
 
 object ServerConfig: BaseConfig() {
-    var linuxBoxIp by stringProperty("10.8.0.5")
+    var linuxBoxIp by stringProperty("10.66.23.100")
     var linuxBoxOrbPort by stringProperty("9999")
     var linuxBoxUsername by stringProperty("at")
     var linuxBoxPassword by stringProperty("gfhjkm")
